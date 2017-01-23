302a2ba8ad93a821642f6c290a806d44a621bc7d
TempleCloud <tim.langford@gmail.com>
2017-01-23 16:29:29 +0000

    Added small script to start a local docker registry.

--- docker/docker-registry/docker-start-docker-registry.sh
+++ docker/docker-registry/docker-start-docker-registry.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-docker run -d -p 5000:5000 --name temos-registry registry:2
+NAME=temos-registry
+
+RUNNING=$(docker ps | grep "${NAME}" | awk -F " " '{print $NF}')
+STOPPED=$(docker ps -a | grep "${NAME}" | awk -F " " '{print $NF}')
+
+if [[ ${RUNNING} == ${NAME} ]]; then
+    echo "container ${NAME} already running, continuing..."
+elif [[ ${STOPPED} == ${NAME} ]]; then
+    echo "container ${NAME} stopped, restarting..."
+    docker start ${NAME}
+else
+    echo "running container ${NAME}"
+    docker run -d -p 5000:5000 --name ${NAME} registry:2
+fi
+
+
