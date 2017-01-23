7caff83415e4a4cc8f4de72efd1bc442d2b251e9
TempleCloud <tim.langford@gmail.com>
2017-01-23 08:31:03 +0000

    Added Plug and YouCompleteMe post processing steps.

--- vim/plug/install-vim-plug-vimrc-file.sh
+++ vim/plug/install-vim-plug-vimrc-file.sh
@@ -2,3 +2,17 @@
 
 # install plug vimrc file
 cp ./vim-plug.vimrc ~/.vimrc
+
+vim -c :PlugInstall
+
+# install vim plugins
+# vim +PlugUpdate +PlugUpgrade +PlugInstall +PlugClean +qa
+vim +PlugInstall +qall
+
+# compile YouCompleteMe plugin if declared
+YCM=$(cat ~/.vimrc | grep Plug 'Valloric/YouCompleteMe')
+if [[ -z ${YCM} ]]; then
+    pushd ~/.vim/plugged/YouCompleteMe
+    ./install.py
+    popd
+fi
