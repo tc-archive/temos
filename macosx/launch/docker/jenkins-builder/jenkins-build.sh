7a0ea2372fa366583a2942d6e9680085b4a750f7
TempleCloud <tim.langford@gmail.com>
2017-02-06 20:40:02 +0000

    Experimenting with multi-container setup.

--- macosx/launch/docker/jenkins-builder/jenkins-build.sh
+++ macosx/launch/docker/jenkins-builder/jenkins-build.sh
@@ -4,6 +4,6 @@ MASTER_IMAGE=jenkins-master
 DATA_IMAGE=jenkins-data
 NGINX_IMAGE=jenkins-nginx
 
-docker build -t temos-${DATA_IMAGE}   ./jenkins-data
-docker build -t temos-${MASTER_IMAGE} ./jenkins-master
-docker build -t temos-${NGINX_IMAGE}  ./jenkins-nginx
+docker build -t temos-${DATA_IMAGE}   ./${DATA_IMAFE}
+docker build -t temos-${MASTER_IMAGE} ./${MASTER_IMAGE}
+docker build -t temos-${NGINX_IMAGE}  ./${NGINX_IMAGE}
