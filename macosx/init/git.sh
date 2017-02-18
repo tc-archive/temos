fc91af909d39d099a4627178558faba646f587ec
TempleCloud <tim.langford@gmail.com>
2017-02-18 13:06:39 +0000

    Added git push stats  alias.

--- macosx/init/git.sh
+++ macosx/init/git.sh
@@ -5,3 +5,13 @@
 if [ -f ~/.git-completion.bash ]; then
     . ~/.git-completion.bash
 fi
+
+# display the list of files that will be pushed on the next push
+function git-push-info() {
+    local _target=${1-origin/master}
+    local _cmd="git diff --stat --cached ${_target}"
+    eval "${_cmd}"
+}
+
+
+alias git-push-info="git-push-info"
