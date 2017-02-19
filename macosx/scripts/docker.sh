031e464557a3207283ba8438e42d2d27e9043e88
TempleCloud <tim.langford@gmail.com>
2017-02-19 15:01:38 +0000

    Added container checking function. Namespaced the functions.

--- macosx/scripts/docker.sh
+++ macosx/scripts/docker.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+#---------------------------------------------------------------------------------------------------
 # docker
 #
-alias docker-clean='unset ${!DOCKER_*}'
-alias moby='screen ~/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty'
+alias temos-docker-clean='unset ${!DOCKER_*}'
+alias temos-docker-moby='screen ~/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty'
 
+#---------------------------------------------------------------------------------------------------
 # Delete all exited docker containers. Optionally, pass in a grep expression to target 
 # specific containers. e.g. 'clean-exited-containers temos', 'clean-exited-containers temos'.
 #
-function clean-exited-containers() {
+function temos::docker-containers-clean-exited() {
     local _targets=$@
     local _cmd="docker ps --filter \"status=exited\" | awk 'NR>1 {print \$1}'"
     if [[ ! -z "${_targets}" ]]; then
@@ -20,6 +22,28 @@ function clean-exited-containers() {
     fi
 }
 alias docker-container-clean="clean-exited-containers"
+
+#---------------------------------------------------------------------------------------------------
+# Function to return the name of the docker container if it exists; else nothing.
+#
+function temos::docker-container-exists() {
+    local _container_name=$1
+    if [[ ! -z "${_container_name}" ]]; then
+        local _has_container_name=$(docker ps -a | awk 'NR>1 {print $NF}' | grep "${_container_name}")
+    fi
+    echo "${_has_container_name}"
+}
+
+#---------------------------------------------------------------------------------------------------
+# Function to return the name of the RUNNING docker container if it exists; else nothing.
+#
+function temos::docker-container-running() {
+    local _container_name=$1
+    if [[ ! -z "${_container_name}" ]]; then
+        local _has_container_name=$(docker ps -a | awk 'NR>1 {print $NF}' | grep "${_container_name}")
+    fi
+    echo "${_has_container_name}"
+}
 
 # docker machine
 #
