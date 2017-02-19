6a8a51cdc77b43e7fb54a732ea8e5ef0dea9320c
TempleCloud <tim.langford@gmail.com>
2017-02-19 10:44:23 +0000

    Tidied up deleted files.

--- macosx/init.sh
+++ macosx/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SCRIPTS_DIR=${TEMOS_HOME}/macosx/init
+SCRIPTS_DIR=${TEMOS_HOME}/macosx/scripts
 
 for file in ${SCRIPTS_DIR}/*.sh
 do
-	# echo `basename ${file}`
+	echo `basename ${file}`
 	source ${file} 	
 done
