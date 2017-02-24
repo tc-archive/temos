0b90a26760f1c1c07ee10d59ccccdf8766516f62
Tim Langford <timothy.langford@oracle.com>
2017-02-24 11:40:58 +0000

    Added a few branch management aliases.

--- macosx/scripts/git.sh
+++ macosx/scripts/git.sh
@@ -7,11 +7,37 @@ if [ -f ~/.git-completion.bash ]; then
 fi
 
 # display the list of files that will be pushed on the next push
-function git-push-info() {
-    local _target=${1-origin/master}
-    local _cmd="git diff --stat --cached ${_target}"
-    eval "${_cmd}"
+function _temos_git_push_info() {
+    local target=${1-origin/master}
+    local cmd="git diff --stat --cached ${target}"
+    eval "${cmd}"
 }
 
+# removes a git branch both locally and remotely
+function _temos_git_remove_branch() {
+    local branch_name=$1
+    if [[ ! -z ${branch_name} ]]; then
+        echo "git push origin --delete ${branch_name}"
+        git push origin --delete ${branch_name}
+        echo "git branch -d ${branch_name}"
+        git branch -d ${branch_name}
+    fi
+}
 
-alias git-push-info="git-push-info"
+# remove locally merged branches. can optinally filter by a simple grep epression.
+function _temos_git_remove_local_merged_branches() {
+    local filter=$1
+    local force_level="-d"
+    if [[ -z ${filter} ]]; then
+        git branch | xargs git branch ${force_level}
+    else:
+        git branch | grep '${filter}' | xargs git branch ${force_level}
+    fi
+}
+
+# git branch | grep 'trjl' | head -n 1 | xargs git branch -d
+
+alias temos-git-push-info="_temos_git_push_info"
+alias temos-git-delete-branch="_temos_git_remove_branch"
+
+
