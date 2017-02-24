2bb33786e3a1a91dbfa955b89a0775d4bf27303e
Tim Langford <timothy.langford@oracle.com>
2017-02-24 11:47:24 +0000

    Adding install script for obmcs python sdk.

---
+++ macosx/install/obmcs-sdk/obmcs-sdk-python.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+VERSION="v1.1.0"
+NAME="oracle-bmcs-python-sdk"
+GITHUB_URL="https://github.com/oracle/bmcs-python-sdk/releases/download/${VERSION}/${NAME}.zip"
+TARGET_DIR="."
+
+CMD="curl -sLo ./${NAME}.zip ${GITHUB_URL}"
+echo "downloading sdk: ${CMD}"
+eval "${CMD}"
+unzip ${NAME}.zip
+
+VIRTUAL_ENV_DIR="venv"
+PYTHON_PATH="/usr/local/bin/python"
+# create virtual environment
+virtualenv -p ${PYTHON_PATH} ${VIRTUAL_ENV}
+source ./venv/bin/activate
+# deactivate
+
+
+# install library
+pushd ${NAME} 
+pip install oraclebmc-*-py2.py3-none-any.whl
+popd
+
+
+# check ssl verison on macosx
+
+# python -c "import ssl; print(ssl.OPENSSL_VERSION)"
+echo "python ssl version: " $(python -c "import ssl; print(ssl.OPENSSL_VERSION)")
+
+
+# brew update
+# brew install openssl
+# brew install pythonbrew install python
