9ca0999f4103a8b336bfd0f34c260622baadd37f
TempleCloud <tim.langford@gmail.com>
2017-03-05 12:05:15 +0000

    Reorganised the launch directory.

---
+++ macosx/install/spinnaker-google/spinnaker-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+https://github.com/GoogleCloudPlatform/spinnaker-deploymentmanager.git
+
+gcloud deployment-manager deployments create --config config.jinja [DEPLOYMENT_NAME] --properties jenkinsPassword=[YOUR_PASSWORD]
+
+gcloud compute instances list | grep spinnaker
+
+gcloud compute ssh [DEPLOYMENT_NAME]-spinnaker-ogo8 --zone us-west1-a -- -L 9000:localhost:9000 -L8084:localhost:8084
+
+gcloud deployment-manager deployments delete [DEPLOYMENT_NAME]
+
+open http://localhost:9000
+
+---
+
+gcloud deployment-manager deployments create --config config.jinja archer --properties jenkinsPassword:woodhouse
+gcloud compute instances list | grep spinnaker
+gcloud compute ssh archer-spinnaker-b280 --zone us-west1-a -- -L 9000:localhost:9000 -L 8084:localhost:8084
+gcloud deployment-manager deployments delete archer
+
+
