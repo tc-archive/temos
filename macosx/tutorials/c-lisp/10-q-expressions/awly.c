981a5229f0ab426a3cbffee658ae4a1904a349fc
TempleCloud <tim.langford@gmail.com>
2017-04-02 22:21:40 +0100

    Added evaluation of q-expressions.

--- macosx/tutorials/c-lisp/10-q-expressions/awly.c
+++ macosx/tutorials/c-lisp/10-q-expressions/awly.c
@@ -231,6 +231,17 @@ lval* lval_take(lval* v, int i) {
     return x;
 }
 
+/* Add the cells of lval 'x' to the cells of lval 'v'.*/
+lval* lval_join(lval* v, lval* x) {
+    // add each cell in x to the cells in v,
+    while(x->count) {
+        v = lval_add(v, lval_pop(x, 0));
+    }
+    // delete the now empty x and return v
+    lval_del(x);
+    return v;
+}
+
 // execute operation
 lval* builtin_op(lval* a, char* op) {
     // ensure all arguments are numbeers
@@ -286,8 +297,76 @@ lval* builtin_op(lval* a, char* op) {
 }
 
 
+
+#define LASSERT(args, cond, err)    \
+    if (cond) {                     \
+        lval_del(args);             \
+        return lval_err(err);       \
+    }
+
+lval* builtin_head(lval* a) {
+    LASSERT(a, a->count != 1, "Function 'head' passed too many arguments.");
+    LASSERT(a, a->cell[0]->type != LVAL_QEXPR, "Function 'head' passed incorrect expression type.");
+    LASSERT(a, a->cell[0]->count == 0, "Function 'head' passed empty list {}.");
+    
+    lval* v = lval_take(a, 0);
+    while (v->count > 1) { lval_del(lval_pop(v, 1)); }
+    return v;
+}
+
+lval* builtin_tail(lval* a) {
+    LASSERT(a, a->count != 1, "Function 'tail' passed too many arguments.");
+    LASSERT(a, a->cell[0]->type != LVAL_QEXPR, "Function 'tail' passed incorrect expression type.");
+    LASSERT(a, a->cell[0]->count == 0, "Function 'tail' passed empty list {}.");
+    
+    lval* v = lval_take(a, 0);
+    lval_del(lval_pop(v, 0));
+    return v;
+}
+
+lval* builtin_list(lval* a) {
+    a->type = LVAL_QEXPR;
+    return a;
+}
+
+lval* builtin_join(lval* a) {
+    for (int i = 0; i < a->count; i++) {
+        LASSERT(a, a->cell[i]->type != LVAL_QEXPR, "Function 'join' passed incorrect expressin type.");
+    }
+
+    lval* x = lval_pop(a, 0);
+
+    while (a->count) {
+        x = lval_join(x, lval_pop(a, 0));
+    }
+
+    lval_del(x);
+    return x;
+}
+
 // declare lval_eval_sexpr
 lval* lval_eval_sexpr(lval* v);
+lval* lval_eval(lval* x);
+
+lval* builtin_eval(lval* a) {
+    LASSERT(a, a->count != 1, "Function 'eval' passed too many arguments.");
+    LASSERT(a, a->cell[0]->type != LVAL_QEXPR, "Function 'eval' passed incorrect expression type.");
+    
+    lval* x = lval_take(a, 0);
+    x->type = LVAL_SEXPR;
+    return lval_eval(x);
+}
+
+lval* builtin(lval* a, char* func) {
+    if (strcmp("list", func) == 0) { return builtin_list(a); }
+    if (strcmp("head", func) == 0) { return builtin_head(a); }
+    if (strcmp("tail", func) == 0) { return builtin_tail(a); }
+    if (strcmp("join", func) == 0) { return builtin_join(a); }
+    if (strcmp("eval", func) == 0) { return builtin_eval(a); }
+    if (strstr("+-/*%^", func)) { return builtin_op(a, func); }
+    lval_del(a);
+    return lval_err("unknown function");
+}
 
 // evaluate lval
 lval* lval_eval(lval* v) {
@@ -328,11 +407,13 @@ lval* lval_eval_sexpr(lval* v) {
         return lval_err("s-expression does not start with a symbol");
     }
     // call builtin with operator
-    lval* result = builtin_op(v, f->sym);
+    lval* result = builtin(v, f->sym);
     lval_del(f);
     return result;
 }
 
+
+// -------------------------------------------------------------------------------------------------
 // main
 int main (int argc, char** argv) {
 
@@ -350,7 +431,8 @@ int main (int argc, char** argv) {
          number   : /-?[0-9]+\\.?[0-9]*/ ;                                      \
          symbol   : '+' | '-' | '*' | '/' | '%' | '^'                           \
                     | \"add\" | \"sub\" | \"mul\" | \"div\" | \"mod\" | \"pow\" \
-                    | \"min\" | \"max\" ;                                       \
+                    | \"min\" | \"max\"                                         \
+                    | \"list\" | \"head\" | \"tail\" | \"join\" | \"eval\" ;    \
          sexpr    : '(' <expr>* ')' ;                                           \
          qexpr    : '{' <expr>* '}' ;                                           \
          expr     : <number> | <symbol> | <sexpr> | <qexpr>  ;                  \
