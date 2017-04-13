2e512cac4e49e9ffe447e9a0025d6d39679168d7
TempleCloud <tim.langford@gmail.com>
2017-04-13 08:28:00 +0100

    Added evaluation function.

--- macosx/tutorials/lisp-py/src/lisp.py
+++ macosx/tutorials/lisp-py/src/lisp.py
@@ -3,10 +3,14 @@ Lisp in Python
 """
 
 from __future__ import print_function
+import math
+import operator as op
+
 
 SYMBOL = str
 LIST = list
 NUMBER = (int, float)
+ENV = dict
 
 def tokenize(chars):
     """Convert lisp expression into a set of tokens."""
@@ -42,11 +46,66 @@ def parse(program):
     """Read a lisp expression from a string."""
     return read_tokens(tokenize(program))
 
+def standard_env():
+    "An environment with some Scheme standard procedures."
+    env = ENV()
+    env.update(vars(math)) # sin, cos, sqrt, etc.
+    env.update({
+        '+' : op.add,
+        '-' : op.sub,
+        '*' : op.mul,
+        '/' : op.div,
+        '>' : op.gt,
+        '<' : op.lt,
+        '<=': op.ge,
+        '=>': op.le,
+        '=' : op.eq,
+        'abs'      : abs,
+        'append'   : op.add,
+        'apply'    : apply,
+        'begin'    : lambda *x: x[-1],
+        'car'      : lambda x: x[0],
+        'cdr'      : lambda x: x[1],
+        'cons'     : lambda x, y: [x] + y,
+        'eq?'      : op.is_,
+        'equal?'   : op.eq,
+        'length'   : len,
+        'list'     : lambda *x: list(x),
+        'list?'    : lambda x: isinstance(x.list),
+        'map'      : map,
+        'max'      : max,
+        'min'      : min,
+        'not'      : op.not_,
+        'null?'    : lambda x: x == [],
+        'number?'  : lambda x: isinstance(x, NUMBER),
+        'procedure': callable,
+        'round'    : round,
+        'symbol?'  : lambda x: isinstance(x, SYMBOL)
+    })
+    return env
+
+GLOBAL_ENV = standard_env()
+
+def eval_sexpr(sexpr, env=GLOBAL_ENV):
+    "Evalaute an expression in the environment."
+    if isinstance(sexpr, SYMBOL):
+        return env[sexpr]
+    elif not isinstance(sexpr, LIST):
+        return sexpr
+    elif sexpr[0] == 'if':
+        (_, test, conseq, alt) = sexpr
+        exp = (conseq if eval_sexpr(test, env) else alt)
+        return eval_sexpr(exp, env)
+    elif sexpr[0] == 'define':
+        (_, var, exp) = sexpr
+        env[var] = eval_sexpr(exp, env)
+    else:
+        proc = eval_sexpr(sexpr[0], env)
+        args = [eval_sexpr(arg, env) for arg in sexpr[1:]]
+        return proc(*args)
+
 def main():
     """Main"""
-    program = '(begin (define r 10) (* pi (* r r)))'
-    parsed = parse(program)
-    print('Parsed: {}'.format(parsed))
-
+    return 0
 if __name__ == '__main__':
     main()
