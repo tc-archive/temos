53059b7647796966be72b3eefd4b4749f2af8fe3
TempleCloud <tim.langford@gmail.com>
2017-06-08 21:57:25 +0100

    Updated provisioners

--- provision/k8s-envs/obmcs-trjl-cluster/init-env.sh
+++ provision/k8s-envs/obmcs-trjl-cluster/init-env.sh
@@ -2,7 +2,6 @@
 
 source ./secret-env.sh
 
-export KUBECONFIG=`pwd`/admin.conf
 
 alias ssh-kube-master="ssh -i ${PRIVATE_KEY} ${USER}@${PUBLIC_IP_MASTER_NODE}"
 alias ssh-kube-01="ssh -i ${PRIVATE_KEY} ${USER}@${PUBLIC_IP_NODE_01}"
@@ -11,3 +10,8 @@ alias ssh-kube-02="ssh -i ${PRIVATE_KEY} ${USER}@${PUBLIC_IP_NODE_02}"
 alias get-cluster-kubeconfig="scp -i ${PRIVATE_KEY} ${USER}@${PUBLIC_IP_MASTER_NODE}:admin.conf ${KUBECONFIG} ."
 
 alias kube-proxy="kubectl proxy"
+
+# configure kubectl
+get-cluster-kubeconfig
+export KUBECONFIG=`pwd`/admin.conf
+
